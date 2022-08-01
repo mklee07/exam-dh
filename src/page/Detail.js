4d22bb9500a0220d25537daafc8a57372c6d35a0
mklee <mklee@innogrid.com>
2022-08-01 10:55:17 +0900

    test

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,10 +1,17 @@
-import { useState,useEffect } from "react";
+import { useState,useEffect,useCallback } from "react";
 import { useParams } from "react-router-dom";
 
+
 function Detail(){
+    // useParams를 활용하여 url에서도 id의 파라미터를 가져와 활용할 수 있게됨
     const {id} = useParams()
     const [movie, setMovie] = useState([]);
     const [loading,setLoading] = useState(true);
+    const [isHovering,setIsHovering] = useState(0)
+    
+    if(isHovering = isHovering(1)){
+        
+    }
 
     useEffect(() => {
         fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
@@ -16,17 +23,67 @@ function Detail(){
           });
       }, []);
 
+    const onClickedJpg = ()=>{
+
+    }
+    const onClickedTitle = useCallback(e => {
+        window.location.href = "/"
+    }, []);
+    
+    const onMouseOver = useCallback (e=>{
+        setIsHovering=(() =>setIsHovering(1))
+    })
+    const onMouseLeave = useCallback (e=>{
+        setIsHovering=(() =>setIsHovering(0))
+    })
+
     return(
         <div>
-            <h1>{movie.title}</h1>
-                <div>
-                    <img src={movie.medium_cover_image}></img>
+            <div style={{ height: '100%', width:'80%' ,float:'right'}}>
+            <tr  width = "100%" height='30%'>
+                            <img src={movie.large_cover_image} onClick={onClickedJpg}></img>
+                        </tr>
+            <div style={{ justifyContent: 'right', height:"100%",width:'75%'}}>
+                <div style={{ width: "100%"}}>
+                    <h2 
+                    onClick={onClickedTitle}
+                    onMouseOver={onMouseOver}
+                    onMouseLeave={onMouseLeave}
+                    data-text-context="true" 
+                    fontWeight="bold" 
+                    fontSize="24px" 
+                    align= 'left'>
+                        {movie.title}
+                    </h2>
+
+                    <table border="1" align="center" height="30%" 
+                    style={{width:'100%', height:'250px', borderCollapse:'collapse', }}>
+                        <tr  width = "100%" height='30%'>
+                            <td bgcolor="whitesmoke" align="center"  ><b>개봉 <br/>연도</b></td>
+                            <td >&nbsp;{movie.year}</td>
+                        </tr>
+                        <tr width = "100%" height='20%'  style={{whiteSpace: 'pre-line'}}>
+                            <td bgcolor="whitesmoke" align="center"><b> rating</b></td>
+                            <td>&nbsp;{movie.rating}</td>
+                        </tr>
+                        <tr width = "100%" height='20%'  style={{whiteSpace: 'pre-line'}}>
+                            <td bgcolor="whitesmoke" align="center"><b> runtime</b></td>
+                            <td>&nbsp;{movie.runtime}분</td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="whitesmoke" align="center"><b>장르</b></td>
+                            <b>&nbsp;{movie.genres[0]}</b>{movie.genres.length >1 && movie.genres.map((genre) => <b key={genre}>,&nbsp;{genre}</b>)}
+                        </tr>
+                        <tr>
+                            <td bgcolor="whitesmoke" align="center"><b>줄거리</b></td>
+                            <td>{movie.description_full}</td>
+                        </tr>
+                        
+                    </table>
                 </div>
-                <h1>year: {movie.year}</h1>
-                <h1>rating: {movie.rating}</h1>
-                <h1>runtime: {movie.runtime}m </h1>
-                <h2>{movie.genres && movie.genres.map((genre) => <li key={genre}>{genre}</li>)}</h2>
+            </div>
         </div>
+    </div>
     )
 }
 export default Detail;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,21 +1,49 @@
-import React, { useState, useCallback, useEffect} from 'react';
+import React, { useState, useCallback, useEffect, useReducer} from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
+import axios from 'axios';
+
+
 
 const Home = () => {
+    axios.get('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
+   const [users, setUsers] = useState();
+   const [loading, setLoading] = useState(false);
+   const [error, setError] = useState(null);
+ 
+   const fetchUsers = async () => {
+     try {
+       // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+       setError(null);
+       setUsers(null);
+       // loading 상태를 true 로 바꿉니다.
+       setLoading(true);
+       const response = await axios.get(
+         'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year'
+       );
+       setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+     } catch (e) {
+       setError(e);
+     }
+     setLoading(false);
+   };
+   useEffect(()=>{
+    fetchUsers();
+    console.log(users)
+   },[])
+
+
    const [rowData,setRowData] = useState([])
-    // var dataState = useState([])
-    // var rowData = dataState[0]
-    // var setData = dataState[1]
-    // setRowData가 밑의 json data의 movies에서 데이터 값을 가져옴.
-
-   const [columnDefs] = useState([
-        { field: 'rating' },
-        { field: 'id' },
-        { field: 'title' }
-   ])
-
+   // var dataState = useState([])
+   // var rowData = dataState[0]
+   // var setData = dataState[1]
+   // setRowData가 밑의 json data의 movies에서 데이터 값을 가져옴.
+  const [columnDefs] = useState([
+       { field: 'rating' },
+       { field: 'id' },
+       { field: 'title' }
+  ])
     //자주 사용됨을써 usecallback을 사용해 최적화함.
     //useCallback은 함수를 재사용하는 hooks.
 
@@ -27,34 +55,6 @@ const Home = () => {
     // window.location.href는 페이지 이동
 
 
-
-
-    useEffect(() => {
-        fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')  //해당 브라우저에 요청을 보냄
-        .then((res) => res.json())      //응답받은 내용을 json으로 불러옴
-        .then((json) => {               //받은 json에서의 data안의 movies를 setrowdata에 불러오고 json을 콘솔로그에 찍음.
-          setRowData(json.data.movies);   
-          console.log(json)
-        });
-        },[]);                          //첫 마운트에
-
-    //fetch( url, [option] ) option에 아무것도 남기지 않으면 GET 메서드가 진행되어 url로부터 컨텐츠 진행. fetch안의 원격서버가 응답하면 .then 아래 코드가 실행됨.
-    //.then(function(response){return response.json()})  //response.json() 응답을 JSON형태로 파싱함.
-
-
-
-        //다른 방법
-        //함수 앞에 async가 붙으면 해당 함수는 항상 프라미스를 반환함.
-    // async function fetchFnc(){
-    //     const json = await(
-    //         await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')
-    //     ).json();
-    //      setMovie(json.data.movie)
-    //      setLoading(false);
-    // };
-    // useEffect(getMovie,[]);
-    // 사용이유 : 프라미스의 result 값을 좀 더 세련되게 얻음(JSON 형태가 아닌 TEXT형태로 받을수 있음). promise.then보다 가독성이 좋음.
-
     
    return (
        <div className="ag-theme-alpine" style={{height: 800, width: 800}}>
@@ -63,7 +63,9 @@ const Home = () => {
                 columnDefs={columnDefs}
                 animateRows={true}
                 rowSelection='multiple'
-                onCellClicked={cellClickedListener}
+                onCellDoubleClicked={cellClickedListener} //onCellClicked -> onCellDoubleClicked으로 바꿈
+                enableCellTextSelection={true} //사용시 아래의 ensureDomOrder을 같이 활용하는게 좋다고 본문에 설명이있음.
+                ensureDomOrder={true}  //본적으로 행과 열은 DOM에서 순서 없이 나타날 수 있고 '잘못된 순서'는 화면 판독기에서 구문 분석할 때 일관되지 않은 결과를 초래할 수 있음. 행 및 열 순서를 강제 실행
            />
        </div>
    );
