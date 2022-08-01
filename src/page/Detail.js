81383a23b31052df908981320743b1321b81de44
kimdonghwan <1489ehdghks@gmail.com>
2022-08-01 19:14:07 +0900

    20220801

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,96 +1,142 @@
-import { useState,useEffect,useCallback } from "react";
+import react, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+//https://getbootstrap.com/docs/5.2/components/buttons/
 
+function Detail() {
+  // useParams를 활용하여 url에서도 id의 파라미터를 가져와 활용할 수 있게됨
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [yt, setYt] = useState();
+  const [genre, setGenre] = useState();
 
-function Detail(){
-    // useParams를 활용하여 url에서도 id의 파라미터를 가져와 활용할 수 있게됨
-    const {id} = useParams()
-    const [movie, setMovie] = useState([]);
-    const [isHovering,setIsHovering] = useState(false)
-    const [yt,setYt] = useState()
-    const isEmpty = (str) => {
-        if(str === "") return true;
-        if(str === null) return true;
-        if(str === undefined) return true;
-    
-        return false;
+  const isEmpty = (str) => {
+    if (str === "") return true;
+    if (str === null) return true;
+    if (str === undefined) return true;
+
+    return false;
+  };
+
+  useEffect(() => {
+    axios(`http://192.168.180.14:3000/movie/getDetail?id=${id}`)
+      .then((res) => res)
+      .then((res) => {
+        setMovie(res.data);
+        setYt(res.data.yt_trailer_code);
+        setGenre(res.data.genres);
+        console.log(res.data.genres);
+        console.log(genre);
+      });
+  }, []);
+
+  function onClickedJpg(e) {
+    console.log(yt);
+    if (!isEmpty(yt)) {
+      window.location.href = `https://www.youtube.com/watch?v=${yt}`;
+    } else {
+      alert("트레일러가 존재하지 않습니다");
     }
+  }
 
+  const onClickedHome = useCallback((e) => {
+    window.location.href = `/`;
+  }, []);
 
-    useEffect(() => {
-        axios(`http://192.168.180.14:3000/movie/getDetail?id=${id}`)
-          .then((res) => res)
-          .then((res) => {
-            setMovie(res.data);
-            setYt(res.data.yt_trailer_code)
-            console.log(yt);
-          });
-      }, []);
+  return (
+    <div>
+      <div style={{ height: "100%", width: "90%", float: "right" }}>
+        <div style={{ justifyContent: "right", height: "100%", width: "90%" }}>
+          <div style={{ width: "100%" }}>
+            <h2
+              className="Home"
+              data-text-context="true"
+              fontWeight="bold"
+              fontSize="24px"
+              align="left"
+            >
+              {movie.title}
+            </h2>
+            <Button
+              type="button"
+              class="btn btn-outline-primary"
+              onClick={onClickedHome}
+              style={{ float: "right" }}
+            >
+              Home
+            </Button>{" "}
+          </div>
 
-    function onClickedJpg(e){
-        console.log(yt)
-          if(!isEmpty(yt)){
-              window.location.href=`https://www.youtube.com/watch?v=${yt}`
-          }else{
-              alert("트레일러가 존재하지 않습니다")
-          }
-    }
-
-
-    const onClickedTitle = useCallback(e => {
-        window.location.href =  `/`
-    }, []);
-    
-    return(
-        <div>
-            <div style={{ height: '100%', width:'80%' ,float:'right' }}>
-            <div style={{ justifyContent: 'right', height:"100%",width:'75%'}}>
-                <div style={{ width: "100%"}}>
-                    <h2 style={{cursor:"pointer" }}
-                    className="Home"
-                    onClick={onClickedTitle}
-                    onMouseOver={()=>setIsHovering(true)}
-                    onMouseLeave={()=>setIsHovering(false)}
-                    data-text-context="true" 
-                    fontWeight="bold" 
-                    fontSize="24px" 
-                    align= 'left'>
-                        {movie.title}
-                    </h2>
-                    <button><src={}></src></button>
-
-                    <table border="1" align="center" height="30%" 
-                    style={{width:'100%', height:'250px', borderCollapse:'collapse', }}>
-                        <tr  width = "100%" height='30%'>
-                            <td bgcolor="whitesmoke" align="center"  ><b>포스트</b></td>
-                            <td bgcolor="whitesmoke" align="center"  ><b>개봉 연도</b></td>
-                            <td bgcolor="whitesmoke" align="center"  ><b>rating</b></td>
-                            <td bgcolor="whitesmoke" align="center"  ><b>runtime</b></td>
-                            <td bgcolor="whitesmoke" align="center"  ><b>장르 </b></td>
-                            <td bgcolor="whitesmoke" align="center"  ><b>줄거리 </b></td>
-                        </tr>
-                        <tr width = "100%" height='20%'  style={{whiteSpace: 'pre-line'}}>
-                            <td align="center" style={{cursor:"pointer"}} onClick={onClickedJpg}><img src={movie.medium_cover_image}></img></td>
-                            <td align="center"><b> &nbsp;{movie.year}</b></td>
-                            <td align="center"><b> &nbsp;{movie.rating}</b></td>
-                            <td align="center"><b> &nbsp;{movie.runtime}분</b></td>
-                            <td align="center"><b> &nbsp;{movie.genres}</b></td>
-
-
-                            {/* <td align="center"><b> </b>{genre.length>0 && genre.map((genre,idx)=>{
-                                let key = genre;
-                                return (<div>&nbsp;{key}</div>)
-                            })} </td> */}
-                            <td align="center"><b> &nbsp;{movie.description_full}</b></td>                            
-                        </tr>
-
-                        
-                    </table>
-                </div>
-            </div>
+          <table
+            align="center"
+            height="100%"
+            style={{
+              width: "100%",
+              height: "250px",
+              borderCollapse: "collapse",
+            }}
+          >
+            <tbody>
+              <tr width="100%" height="10%">
+                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                  <b>포스트</b>
+                </td>
+                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                  <b>개봉 연도</b>
+                </td>
+                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                  <b>rating</b>
+                </td>
+                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                  <b>runtime</b>
+                </td>
+                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                  <b>장르 </b>
+                </td>
+                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                  <b>줄거리 </b>
+                </td>
+              </tr>
+              <tr width="100%" height="20%" style={{ whiteSpace: "pre-line" }}>
+                <td
+                  bgcolor="#f0f4f6"
+                  align="center"
+                  style={{ cursor: "pointer" }}
+                  onClick={onClickedJpg}
+                >
+                  <img src={movie.small_cover_image}></img>
+                </td>
+                <td align="center" bgcolor="#f0f4f6">
+                  <b> &nbsp;{movie.year}</b>
+                </td>
+                <td align="center" bgcolor="#f0f4f6">
+                  <b> &nbsp;{movie.rating}</b>
+                </td>
+                <td align="center" bgcolor="#f0f4f6">
+                  <b> &nbsp;{movie.runtime}분</b>
+                </td>
+                <td align="center" bgcolor="#f0f4f6">
+                  <>{genre.replaceAll("{", "").replaceAll("}", "")}</>
+                </td>
+                <td align="center" bgcolor="#f0f4f6">
+                  <b> &nbsp;{movie.description_full}</b>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <h6>포스터를 클릭시 트레일러로 넘어갑니다</h6>
         </div>
+      </div>
     </div>
-    )
+  );
 }
 export default Detail;
+
+{
+  /* <td align="center"><b> </b>{genre.length>0 && genre.map((genre,idx)=>{
+                                let key = genre;
+                                return (<div>&nbsp;{key}</div>)
+                            })} </td> */
+}
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,57 +1,56 @@
-import React, { useState, useCallback, useEffect, useReducer} from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-import axios from 'axios';
-
+import React, { useState, useCallback, useEffect, useReducer } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import axios from "axios";
 
 const Home = () => {
-   const [rowData,setRowData] = useState([])
-   // var dataState = useState([])
-   // var rowData = dataState[0]
-   // var setData = dataState[1]
-   // setRowData가 밑의 json data의 movies에서 데이터 값을 가져옴.
+  const [rowData, setRowData] = useState([]);
+  // var dataState = useState([])
+  // var rowData = dataState[0]
+  // var setData = dataState[1]
+  // setRowData가 밑의 json data의 movies에서 데이터 값을 가져옴.
   const [columnDefs] = useState([
-       { field: 'rating' },
-       { field: 'year' },
-       { field: 'title' }
-  ])
-    //자주 사용됨을써 usecallback을 사용해 최적화함.
-    //useCallback은 함수를 재사용하는 hooks.
+    { field: "rating" },
+    { field: "year" },
+    { field: "title" },
+  ]);
+  //자주 사용됨을써 usecallback을 사용해 최적화함.
+  //useCallback은 함수를 재사용하는 hooks.
 
-    const cellClickedListener = useCallback(e => {
-        window.location.href = "/" + "movie" + "/" + e.data.id;
-    }, []);
-    // const cellClickedListener = useCallback( function (e){return {window.location.href = "/"+ "movie" + "/" + e.data.id}} )
-    // 버튼이 클릭 될 때마다 useCallback이 작동하여 함수를 재사용함.
-    // window.location.href는 페이지 이동
+  const cellClickedListener = useCallback((e) => {
+    window.location.href = "/" + "movie" + "/" + e.data.id;
+  }, []);
+  // const cellClickedListener = useCallback( function (e){return {window.location.href = "/"+ "movie" + "/" + e.data.id}} )
+  // 버튼이 클릭 될 때마다 useCallback이 작동하여 함수를 재사용함.
+  // window.location.href는 페이지 이동
 
-    useEffect(()=>{
-    axios.get('http://192.168.180.14:3000/movie/getMovies')
-    .then((res) => res)
-    .then((res) =>{
-      setRowData(res.data)
-      console.log(res.data)
-    });
-  },[]);
-    
-   return (
-       <div className="ag-theme-alpine" style={{height: 800, width: 800}}>
-           <AgGridReact
-                rowData={rowData}
-                columnDefs={columnDefs}
-                animateRows={true}
-                rowSelection='multiple'
-                onCellDoubleClicked={cellClickedListener} //onCellClicked -> onCellDoubleClicked으로 바꿈
-                enableCellTextSelection={true} //사용시 아래의 ensureDomOrder을 같이 활용하는게 좋다고 본문에 설명이있음.
-                ensureDomOrder={true}  //본적으로 행과 열은 DOM에서 순서 없이 나타날 수 있고 '잘못된 순서'는 화면 판독기에서 구문 분석할 때 일관되지 않은 결과를 초래할 수 있음. 행 및 열 순서를 강제 실행
-           />
-       </div>
-   );
+  useEffect(() => {
+    axios
+      .get("http://192.168.180.14:3000/movie/getMovies")
+      .then((res) => res)
+      .then((res) => {
+        setRowData(res.data);
+        console.log(res.data);
+      });
+  }, []);
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 800, width: 800 }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        animateRows={true}
+        rowSelection="multiple"
+        onCellDoubleClicked={cellClickedListener} //onCellClicked -> onCellDoubleClicked으로 바꿈
+        enableCellTextSelection={true} //사용시 아래의 ensureDomOrder을 같이 활용하는게 좋다고 본문에 설명이있음.
+        ensureDomOrder={true} //본적으로 행과 열은 DOM에서 순서 없이 나타날 수 있고 '잘못된 순서'는 화면 판독기에서 구문 분석할 때 일관되지 않은 결과를 초래할 수 있음. 행 및 열 순서를 강제 실행
+      />
+    </div>
+  );
 };
 
 export default Home;
-
 
 //https://yts.mx/api/v2/movie_details.json?movie_id=37384
 //http://192.168.180.14:3000/movie/getMovies
