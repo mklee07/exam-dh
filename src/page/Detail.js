165779ba0980893a4080b8d0ab870b525d09d9fe
kimdonghwan <1489ehdghks@gmail.com>
2022-08-03 10:16:21 +0900

    0803

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -2,40 +2,39 @@ import react, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Detail.css";
 import { Button } from "react-bootstrap";
 //https://getbootstrap.com/docs/5.2/components/buttons/
+//bootstrap에서 버튼을 가져와 사용했음.
 
 function Detail() {
-  // useParams를 활용하여 url에서도 id의 파라미터를 가져와 활용할 수 있게됨
   const { id } = useParams();
   const [movie, setMovie] = useState([]);
-  const [yt, setYt] = useState();
-  const [genre, setGenre] = useState();
 
+  //값이 "", null , undefined 일 경우 원치않는 에러를 불러올 수 있기에 이들을 감지하는 isEmpty를 선언하여 사용함. (onclickJpg, genre에서 사용중.)
   const isEmpty = (str) => {
     if (str === "") return true;
     if (str === null) return true;
     if (str === undefined) return true;
-
     return false;
   };
 
+  //데이터 값이 {}으로 덮혀있어 usestate에도 usestate({})으로 덮었지만 object가 나와 yt_trailer_code 값을 못읽어 따로 yt를 만들었음, usestate()로 고치니 해결됨
   useEffect(() => {
     axios(`http://192.168.180.14:3000/movie/getDetail?id=${id}`)
-      .then((res) => res)
+      .then((res) => res.data)
       .then((res) => {
-        setMovie(res.data);
-        setYt(res.data.yt_trailer_code);
-        setGenre(res.data.genres);
-        console.log(res.data.genres);
-        console.log(genre);
+        setMovie(res);
+        console.log(res.genres);
       });
   }, []);
 
+  //react에서 _를 잘 못읽는걸로 알고 있어서 따로 yt를 usestate로 이용해 res.data.yt_trailer_code를 state에 지정해서 사용했지만 아래와 같이 사용해도 잘 읽어져서 그대로 사용함.
   function onClickedJpg(e) {
-    console.log(yt);
-    if (!isEmpty(yt)) {
-      window.location.href = `https://www.youtube.com/watch?v=${yt}`;
+    console.log(movie.yt_trailer_code);
+    // isEmpty를 활용하여 "", null, undefined를 검사하고 트레일러코드가 "",null,undefined면 알람이 나오고 그 외의 경우엔 유튜브로 통해 트레일러를 볼 수 있게 사용함.
+    if (!isEmpty(movie.yt_trailer_code)) {
+      window.location.href = `https://www.youtube.com/watch?v=${movie.yt_trailer_code}`;
     } else {
       alert("트레일러가 존재하지 않습니다");
     }
@@ -48,85 +47,115 @@ function Detail() {
   return (
     <div>
       <div style={{ height: "100%", width: "90%", float: "right" }}>
-        <div style={{ justifyContent: "right", height: "100%", width: "90%" }}>
-          <div style={{ width: "100%" }}>
+        <div
+          className="main"
+          style={{ justifyContent: "right", width: "90%", alignItems: "" }}
+        >
+          <div className="header" style={{ width: "100%" }}>
             <h2
+              id="title"
               className="Home"
+              style={{ paddingTop: "20px" }}
               data-text-context="true"
-              fontWeight="bold"
-              fontSize="24px"
               align="left"
             >
               {movie.title}
+              {/* bootstrap에서 class를 지정하여 버튼을 사용함.  */}
+              <Button
+                type="button"
+                class="btn btn-outline-primary"
+                onClick={onClickedHome}
+                style={{ float: "right", paddingBottom: "10px" }}
+              >
+                Home
+              </Button>
             </h2>
-            <Button
-              type="button"
-              class="btn btn-outline-primary"
-              onClick={onClickedHome}
-              style={{ float: "right" }}
-            >
-              Home
-            </Button>{" "}
           </div>
 
           <table
             align="center"
-            height="100%"
             style={{
               width: "100%",
-              height: "250px",
               borderCollapse: "collapse",
             }}
           >
             <tbody>
-              <tr width="100%" height="10%">
-                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
-                  <b>포스트</b>
-                </td>
-                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+              <tr>
+                <td className="FirstCell" style={{ width: "15%" }}>
                   <b>개봉 연도</b>
                 </td>
-                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                <td className="FirstCell">
                   <b>rating</b>
                 </td>
-                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                <td className="FirstCell" style={{ width: "25%" }}>
                   <b>runtime</b>
                 </td>
-                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
+                <td className="FirstCell" style={{ width: "35%" }}>
                   <b>장르 </b>
                 </td>
-                <td bgcolor="#0090ff" style={{ color: "white" }} align="center">
-                  <b>줄거리 </b>
-                </td>
               </tr>
-              <tr width="100%" height="20%" style={{ whiteSpace: "pre-line" }}>
-                <td
-                  bgcolor="#f0f4f6"
-                  align="center"
-                  style={{ cursor: "pointer" }}
-                  onClick={onClickedJpg}
-                >
-                  <img src={movie.small_cover_image}></img>
-                </td>
-                <td align="center" bgcolor="#f0f4f6">
+              <tr
+                width="100%"
+                align="center"
+                style={{ whiteSpace: "pre-line" }}
+              >
+                <td className="SecondCell">
                   <b> &nbsp;{movie.year}</b>
+                  {/* &nbsp;는 빈칸을 나타남 */}
                 </td>
-                <td align="center" bgcolor="#f0f4f6">
+                <td className="SecondCell">
                   <b> &nbsp;{movie.rating}</b>
                 </td>
-                <td align="center" bgcolor="#f0f4f6">
+                <td className="SecondCell">
                   <b> &nbsp;{movie.runtime}분</b>
                 </td>
-                <td align="center" bgcolor="#f0f4f6">
-                  <>{genre.replaceAll("{", "").replaceAll("}", "")}</>
-                </td>
-                <td align="center" bgcolor="#f0f4f6">
-                  <b> &nbsp;{movie.description_full}</b>
+                <td className="SecondCell">
+                  <b>
+                    {!isEmpty(movie.genres) &&
+                      movie.genres.replaceAll("{", "").replaceAll("}", "")}
+                    {/* genre만 하니 랜더링 할때마다 replaceAll 프로퍼티를 못읽다라는 에러가 나왔었음. &&조건문을 활용하니 해결됨. isEmpty를 사용해 "",undefined,null을 검사함 */}
+                    {/* replaceAll :  변경하고 싶은 문자열.replaceAll("변경이 될 문자" , "변경하고 싶은 문자") 처럼 사용하면 됨.   */}
+                  </b>
                 </td>
               </tr>
             </tbody>
           </table>
-          <h6>포스터를 클릭시 트레일러로 넘어갑니다</h6>
+          {/* 위는 테이블 형식이라 테이블 안에 넣을시 개봉 연도 너비와 같은 너비로 사용이 된다. */}
+          <tfoot width="100%">
+            <tr width="40%">
+              <td
+                className="FirstCell"
+                bgcolor="#0090ff"
+                style={{ color: "white", width: "40%" }}
+                align="center"
+              >
+                <b>포스트</b>
+              </td>
+              <td
+                className="FirstCell"
+                bgcolor="#0090ff"
+                style={{ color: "white", width: "60%" }}
+                align="center"
+              >
+                <b>줄거리 </b>
+              </td>
+            </tr>
+            <tr width="100%">
+              <td bgcolor="#f0f4f6" align="center">
+                <img
+                  style={{ cursor: "pointer", paddingTop: "10px" }}
+                  onClick={onClickedJpg}
+                  src={movie.medium_cover_image}
+                ></img>
+                <h6 style={{ color: "#0090ff", paddingTop: "5px" }}>
+                  *포스터를 클릭시 트레일러로 넘어갑니다
+                </h6>
+              </td>
+              <td bgcolor="#f0f4f6">
+                <b> - {movie.description_full}</b>
+              </td>
+            </tr>
+          </tfoot>
         </div>
       </div>
     </div>
@@ -135,8 +164,11 @@ function Detail() {
 export default Detail;
 
 {
-  /* <td align="center"><b> </b>{genre.length>0 && genre.map((genre,idx)=>{
+  /* 장르 반복
+  <td align="center"><b> </b>{genre.length>0 && genre.map((genre,idx)=>{
                                 let key = genre;
                                 return (<div>&nbsp;{key}</div>)
                             })} </td> */
 }
+
+// replaceall : <b>{genre.replaceAll("{", "").replaceAll("}", "")}</b>
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,49 +1,120 @@
-import React, { useState, useCallback, useEffect, useReducer } from "react";
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useMemo,
+} from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import axios from "axios";
+import { Button } from "react-bootstrap";
 
 const Home = () => {
-  const [rowData, setRowData] = useState([]);
-  // var dataState = useState([])
-  // var rowData = dataState[0]
-  // var setData = dataState[1]
-  // setRowData가 밑의 json data의 movies에서 데이터 값을 가져옴.
+  const gridRef = useRef();
+  const [rowData, setRowData] = useState();
   const [columnDefs] = useState([
-    { field: "rating" },
-    { field: "year" },
-    { field: "title" },
+    { field: "rating", resizable: true, filter: "agNumberColumnFilter" }, //resizeable은 옆의 선을 두어 크기를 맘대로 조절 가능, filter는
+    { field: "year", resizable: true, filter: "agNumberColumnFilter" },
+    {
+      field: "title",
+      resizable: true,
+      filter: "agSetColumnFilter",
+    },
+    { field: "genres", resizable: true, filter: "agSetColumnFilter" },
+    { field: "runtime", resizable: true, filter: "agNumberColumnFilter" },
   ]);
-  //자주 사용됨을써 usecallback을 사용해 최적화함.
-  //useCallback은 함수를 재사용하는 hooks.
 
   const cellClickedListener = useCallback((e) => {
     window.location.href = "/" + "movie" + "/" + e.data.id;
   }, []);
-  // const cellClickedListener = useCallback( function (e){return {window.location.href = "/"+ "movie" + "/" + e.data.id}} )
-  // 버튼이 클릭 될 때마다 useCallback이 작동하여 함수를 재사용함.
-  // window.location.href는 페이지 이동
 
+  const sizeToFit = useCallback(() => {
+    gridRef.current.api.sizeColumnsToFit();
+  }, []);
+
+  const onClickedHome = useCallback((e) => {
+    window.location.href = `/`;
+  }, []);
+
+  // patch에서 axios로 변경.
+  // fetch는 필요한 데이터가 JSON 데이터 포맷이 아니기 떄문에 응답 객체의 .json() 메서드로 호출함.
+  // axios는 응답 데이터를 기본적으로 JSON 타입으로 사용할 수 있고 응답 데이터는 언제나 응답 객체의 data 프로퍼티에서 사용할 수 있음.
   useEffect(() => {
     axios
       .get("http://192.168.180.14:3000/movie/getMovies")
-      .then((res) => res)
+      .then((res) => res.data)
       .then((res) => {
-        setRowData(res.data);
-        console.log(res.data);
+        setRowData(res);
+        console.log(res);
       });
   }, []);
 
+  const autoSizeAll = useCallback((skipHeader) => {
+    const allColumnIds = [];
+    gridRef.current.columnApi.getColumns().forEach((column) => {
+      allColumnIds.push(column.getId());
+    });
+    gridRef.current.columnApi.autoSizeColumns(allColumnIds, skipHeader);
+  }, []);
+
   return (
-    <div className="ag-theme-alpine" style={{ height: 800, width: 800 }}>
+    <div className="ag-theme-alpine" style={{ height: 800, width: 1920 }}>
+      <div className="header" style={{ alignItems: "center" }}>
+        <Button
+          style={{ float: "left" }}
+          type="button"
+          class="btn btn-outline-primary"
+          onClick={() => autoSizeAll(false)}
+        >
+          자동 정렬
+        </Button>
+        <Button
+          style={{ float: "left" }}
+          type="button"
+          class="btn btn-outline-primary"
+          onClick={sizeToFit}
+        >
+          셀 크기에 맞게 정렬
+        </Button>
+        <h3
+          onClick={onClickedHome}
+          style={{
+            float: "left",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            cursor: "pointer",
+          }}
+        >
+          Home
+        </h3>
+        <div class="input-group mb-3" style={{ width: "30%" }}>
+          <input
+            style={{ width: "40%" }}
+            type="text"
+            class="form-control"
+            placeholder="검색할 내용"
+          />
+          <button
+            class="btn btn-outline-secondary"
+            style={{ float: "right" }}
+            type="button"
+            id="button-addon2"
+          >
+            검색
+          </button>
+        </div>
+      </div>
+
       <AgGridReact
+        ref={gridRef}
         rowData={rowData}
         columnDefs={columnDefs}
         animateRows={true}
         rowSelection="multiple"
         onCellDoubleClicked={cellClickedListener} //onCellClicked -> onCellDoubleClicked으로 바꿈
-        enableCellTextSelection={true} //사용시 아래의 ensureDomOrder을 같이 활용하는게 좋다고 본문에 설명이있음.
+        enableCellTextSelection={true} //  사용시 아래의 ensureDomOrder을 같이 활용하는게 좋다고 본문에 설명이있음.
         ensureDomOrder={true} //본적으로 행과 열은 DOM에서 순서 없이 나타날 수 있고 '잘못된 순서'는 화면 판독기에서 구문 분석할 때 일관되지 않은 결과를 초래할 수 있음. 행 및 열 순서를 강제 실행
       />
     </div>
@@ -54,3 +125,4 @@ export default Home;
 
 //https://yts.mx/api/v2/movie_details.json?movie_id=37384
 //http://192.168.180.14:3000/movie/getMovies
+//<h3 onClick={onClickedHome} style={{ float: "right" }}>
